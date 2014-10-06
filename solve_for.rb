5032af93aab883be224b0d370a80ad29d1acb19f
Ricky Bacci <brownieelf175@gmail.com>
2014-10-05 22:05:35 -0400

    remove old code

--- solve_for.rb
+++ solve_for.rb
@@ -11,14 +11,3 @@ def solve_for_two(array)
   solve_for_columns( array, :solve_for_twos )
 end
 
-def solve_for_three(array)
-  # solve_for_rows( array, :solve_for_threes )
-  # solve_for_boxes( array, :solve_for_threes )
-  # solve_for_columns( array, :solve_for_threes )
-end
-
-def solve_for_four(array, section)
-  # solve_for_boxes( array, :solve_for_fours )
-  # solve_for_rows( array, :solve_for_fours )
-  # solve_for_columns( array, :solve_for_fours )
-end
