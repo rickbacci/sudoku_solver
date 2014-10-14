ae73b6e3a86f3bdcd1a6df4029cc513d1d280cda
Ricky Bacci <brownieelf175@gmail.com>
2014-10-14 10:09:36 -0400

    refactor total_numbers_remaining

--- total_numbers_remaining.rb
+++ total_numbers_remaining.rb
@@ -1,32 +1,34 @@
-def total_numbers_remaining?(array)
-  @number_totals_remaining = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+def build_hash_of_numbers_remaining(number_totals_remaining)
+  hash = {}
 
-  temp_array = []
+  number_totals_remaining.each_with_index do |number, index|
+    # p "There are #{number} #{index + 1}'s remaining in the puzzle"
+    hash[index + 1] = number
+    puts if index == 8
+  end
+  hash.sort { |a1, a2| a1[1] <=> a2[1] }
+end
+
+def sort_values(order)
+  new_order = []
+
+  order.each do |o|
+    new_order << o[0]
+  end
+  new_order
+end
+
+def total_numbers_remaining?(array)
+  number_totals_remaining = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
   array.each do |row|
     row.each do |element|
       next if element.is_a?(Integer)
-      temp_array << element
       (1..9).each_with_index do |num, index|
-        @number_totals_remaining[index] += 1 if
-        element.flatten.include?(num)
+        number_totals_remaining[index] += 1 if element.flatten.include?(num)
       end
     end
   end
-  hash = {}
-
-  @number_totals_remaining.each_with_index do |number, index|
-    # p "There are #{number} #{index + 1}'s remaining in the puzzle"
-    hash[index + 1] = number
-    # puts if index == 8
-  end
-
-  order = hash.sort { |a1, a2| a1[1] <=> a2[1] }
-
-  @new_order = []
-
-  order.each do |o|
-    @new_order << o[0]
-  end
-  @new_order
+  p order = build_hash_of_numbers_remaining(number_totals_remaining)
+  sort_values(order)
 end
