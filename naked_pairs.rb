78f03daf013233d43b5aa2edd6f15c1ca299aa50
Ricky Bacci <brownieelf175@gmail.com>
2014-10-17 13:44:03 -0400

    create naked_pairs from solve_for_two

---
+++ naked_pairs.rb
@@ -0,0 +1,50 @@
+def naked_pairs(array)
+  solve_for_boxes(array, :naked_pair)
+  solve_for_rows(array, :naked_pair)
+  solve_for_columns(array, :naked_pair)
+end
+
+def naked_pair(array, rows, columns, _location)
+  temp_array = Common.build_temp_array(array, rows, columns)
+
+  temp_array.each do |element|
+    next if element.size != 2 || temp_array.count(element) != 2
+    pair = element
+    temp_array.each do |element2|
+      if element2.is_a?(Array) && pair != element2
+        before = element2
+        clear_naked_pair(array, rows, columns, element, element2, pair)
+      end
+    end
+  end
+end
+
+def clear_naked_pair(array, rows, columns, element, element2, value)
+  rows.each do |r|
+    columns.each do |c|
+
+      if array[r][c].is_a?(Array) && clear?(array[r][c], element, element2, value)
+        before = array[r][c]
+
+        # this needs fixed....should not be allowing matches.size > 3
+        array[r][c] -= value
+        
+
+        if before != array[r][c]
+          history << "[#{r}][#{c}] clearing pair: #{value}...element before: #{before}," \
+                      " element after: #{array[r][c]}"
+        end
+      end
+    end
+  end
+  Common.clear_all(array)
+  value = []
+end
+
+
+def clear?(val, el1, el2, value)
+  return false if val == el1
+  return false if val == el2
+  return false if val == value
+  true
+end
