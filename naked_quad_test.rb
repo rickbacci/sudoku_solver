7348aa19bcf7b62e9261b8420bd3228de59cbc7e
Ricky Bacci <brownieelf175@gmail.com>
2014-10-20 14:30:40 -0400

    convert naked_quad_test to methods for program

--- naked_quad_test.rb
+++ naked_quad_test.rb
@@ -14,34 +14,40 @@ array = [8, [1, 6, 7], [4, 5], [3, 5, 6], [2, 3, 7, 9], [2, 5, 7], [2, 7], [1, 9
 # p ([4,5] + [1, 9]).uniq
 # p ([4,5] + [4, 7]).uniq
 
-quads = []
-array.each do |arr|
-  next if arr.is_a?(Integer)
-  array.each do |val|
-    next if val.is_a?(Integer)
+def possible_combinations(array)
+  quads = []
+  array.each do |arr|
+    next if arr.is_a?(Integer)
+    array.each do |val|
+      next if val.is_a?(Integer)
 
-    quad = (arr + val).uniq
-    quads << quad if quad.length == 4
+      quad = (arr + val).uniq
+      quads << quad if quad.length == 4
+    end
   end
-end
-p possible_quads = quads.uniq.sort!
-puts
-possible_quads.each do |p|
-  p p.sort!
+  quads.uniq.sort
 end
 
-puts
-found_quad = []
-possible_quads.each do |quad|
-  array.each do |element|
-    next if element.is_a?(Integer)
-    found_quad << element if (element - quad) == []
-  end
-  if found_quad.length == 4
-    p found_quad
-    p quad
-  end
+possible_quads = possible_combinations(array)
+
+# puts
+# possible_quads.each do |p|
+#   p p.sort!
+# end
+
+
+def naked_quad?(array, possible_quads)
   found_quad = []
+  possible_quads.each do |quad|
+    array.each do |element|
+      next if element.is_a?(Integer)
+      found_quad << element if (element - quad) == []
+    end
+    
+    return quad.sort if found_quad.length == 4
+    found_quad = []
+    nil
+  end
 end
 
-  
+p naked_quad?(array, possible_quads)
