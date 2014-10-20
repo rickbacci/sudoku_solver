4a7b5533cc19eadeee4f1e4ae27f51f2168fd792
Ricky Bacci <brownieelf175@gmail.com>
2014-10-20 14:42:06 -0400

    naked_quads working for all combinations

--- naked_quads.rb
+++ naked_quads.rb
@@ -4,16 +4,50 @@ def naked_quads(array)
   solve_for_columns(array, :naked_quad)
 end
 
-def naked_quad?(quad, temp_array)
-  values = []
-  temp_array.each do |element|
-    next if element.is_a?(Integer)
-    values << element if (element - quad) == []
-  end
+def possible_combinations(array)
+  quads = []
+  array.each do |arr|
+    next if arr.is_a?(Integer)
+    array.each do |val|
+      next if val.is_a?(Integer)
 
-  return quad if values.length == quad.length
-  nil
+      quad = (arr + val).uniq
+      quads << quad if quad.length == 4
+    end
+  end
+  quads.uniq.sort
 end
+
+# possible_quads = possible_combinations(array)
+
+def naked_quad?(array, possible_quads)
+  found_quad = []
+  possible_quads.each do |quad|
+    array.each do |element|
+      next if element.is_a?(Integer)
+      found_quad << element if (element - quad) == []
+    end
+    
+    return quad.sort if found_quad.length == 4
+    found_quad = []
+    nil
+  end
+end
+
+# p naked_quad?(array, possible_quads)
+
+
+
+# def naked_quad?(quad, temp_array)
+#   values = []
+#   temp_array.each do |element|
+#     next if element.is_a?(Integer)
+#     values << element if (element - quad) == []
+#   end
+
+#   return quad if values.length == quad.length
+#   nil
+# end
 
 def remaining_numbers(quad)
   [1, 2, 3, 4, 5, 6, 7, 8, 9] - quad
@@ -46,13 +80,15 @@ end
 
 def naked_quad(array, rows, columns, location)
   temp_array = Common.build_temp_array(array, rows, columns)
+  possible_quads = possible_combinations(temp_array)
+
   # [2, 4, 5, 7]
   # [8, [1, 6, 7], [4, 5], [3, 5, 6], [2, 3, 7, 9], [2, 5, 7], [2, 7], [1, 9], [4, 7]]
   rows.each do |row|
     columns.each do |column|
 
-      element = array[row][column]
-      quad = naked_quad?(element, temp_array) if element.size == 4
+      #element = array[row][column]
+      quad = naked_quad?(temp_array, possible_quads) # if element.size == 4
 
       next if quad.nil?
 
