77fea2203936626940a76e27d0e85c03c486f3b2
Ricky Bacci <brownieelf175@gmail.com>
2014-10-20 10:25:28 -0400

    refactor naked_pairs

--- naked_pairs.rb
+++ naked_pairs.rb
@@ -4,47 +4,58 @@ def naked_pairs(array)
   solve_for_columns(array, :naked_pair)
 end
 
+def naked_pair?(pair, temp_array)
+  values = []
+  temp_array.each do |element|
+    next if element.is_a?(Integer)
+    values << element if (element - pair) == []
+  end
+
+  return pair if values.length == pair.length
+  nil
+end
+
+def remaining_numbers(pair)
+  [1, 2, 3, 4, 5, 6, 7, 8, 9] - pair
+end
+
+def location
+  @location
+end
+
+def clear_naked_pair(array, rows, columns, pair, location)
+  remaining_numbers(pair).each do |number|
+    rows.each do |row|
+      columns.each do |column|
+
+        element = array[row][column]
+
+        next unless element.is_a?(Array) && element.include?(number)
+
+        unless element == (element - pair)
+          history << "#{location} [#{row}][#{column}]       :naked_pair --- " \
+                   "clearing #{pair} before: #{element} after: #{element -= pair}"
+        end
+
+        Common.clear_all(array)
+        array[row][column] = element
+      end
+    end
+  end
+end
+
 def naked_pair(array, rows, columns, location)
   temp_array = Common.build_temp_array(array, rows, columns)
 
-  temp_array.each do |element|
-    next if element.size != 2 || temp_array.count(element) != 2
-    pair = element
-    temp_array.each do |element2|
-      if element2.is_a?(Array) && pair != element2
-        before = element2
-        clear_naked_pair(location, array, rows, columns, element, element2, pair)
-      end
+  rows.each do |row|
+    columns.each do |column|
+
+      element = array[row][column]
+      pair = naked_pair?(element, temp_array) if element.size == 2
+
+      next if pair.nil?
+
+      clear_naked_pair(array, rows, columns, pair, location)
     end
   end
-end
-
-def clear_naked_pair(location, array, rows, columns, element, element2, value)
-  rows.each do |r|
-    columns.each do |c|
-
-      if array[r][c].is_a?(Array) && clear?(array[r][c], element, element2, value)
-        before = array[r][c]
-
-        # this needs fixed....should not be allowing matches.size > 3
-        array[r][c] -= value
-        Common.clear_all(array)
-
-
-        if before != array[r][c]
-          history << "#{location} [#{r}][#{c}]       :naked_pair --- clearing #{value} " \
-                      "before: #{before}, element after: #{array[r][c]}"
-        end
-      end
-    end
-  end
-  # Common.clear_all(array)
-  value = [] # why?
-end
-
-def clear?(val, el1, el2, value)
-  return false if val == el1
-  return false if val == el2
-  return false if val == value
-  true
 end
